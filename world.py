fae5a942ddc20173421de5debc6da2cb83124948
robert <yarnoiser@gmail.com>
2017-04-14 16:28:35 -0600

    tiles and scenes

--- world.py
+++ world.py
@@ -4,7 +4,35 @@ class Tile():
     self.clip = clip
     self.contents = contents
 
-def Empty_tile():
-  return Tile(None, clip=true)
+def EmptyTile():
+  return Tile(None, clip=True)
+
+class Scene():
+  def __init__(self, width, height):
+    self.tiles = [[EmptyTile() for tile in range(height)] for column in range(width)]
+
+  def setTile(self, x, y, tile):
+    self.tiles[x, y] = tile
+
+  def add(self, obj, x, y):
+    self.tiles[x, y].contents.append(obj)
+
+  def locate(self, obj):
+    for x in self.tiles:
+      for y in x:
+        for o in self.tiles[x][y].contents:
+          if o is obj:
+            return x, y
+    return None, None
+
+  def remove(self, obj, x=None, y=None):
+    if not (x and y):
+      x, y = self.locate(obj)
+    if not (x and y):
+      return
+    self.tiles[x][y].contents = filter(lambda o: not obj is o, self.tiles[x][y].contents)
+
+def EmptyScene():
+  return Scene(0, 0)
 
 
