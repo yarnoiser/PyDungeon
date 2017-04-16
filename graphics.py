c37ad314605065194732202539db50eef94ea3da
Robert Smiley <yarnoiserdev@gmail.com>
2017-04-16 01:58:08 -0600

    bug fix for texture loading

--- graphics.py
+++ graphics.py
@@ -5,15 +5,16 @@ import sdl2.ext
 
 sdl2.ext.init()
 window = sdl2.ext.Window("PyDungeon", size=(640, 480))
-window.show()
 
 textures = {}
 
 for f in os.listdir("./Textures"):
-  if os.path.isfile(f):
-    textures[os.path.splitext(f)[0]] = sdl2.ext.load_image(f)
+  fullPath = "./Textures/" + f
+  if os.path.isfile(fullPath):
+    textures[os.path.splitext(f)[0]] = sdl2.ext.load_image(fullPath)
 
 def loop():
+  window.show()
   while 1:
     events = sdl2.ext.get_events()
     for event in events:
